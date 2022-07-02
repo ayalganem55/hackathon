60b825e93d010ba74b2ff664c9703de4bb319446
iftibar <agol.clinic@gmail.com>
2022-07-02 21:00:37 +0300

    fix

--- app/forms.py
+++ app/forms.py
@@ -1,17 +1,17 @@
 from flask_wtf import FlaskForm
 from wtforms.validators import data_required, ValidationError
-from wtforms import StringField, SubmitField, EmailField, TelField, URLField, PasswordField
+from wtforms import StringField, SubmitField, EmailField, TelField, URLField
 from flask_wtf.file import FileField
 
 
 class AddNewBusiness(FlaskForm):
-	name = StringField('name of business', validators=[data_required()])
+	name = StringField('name', validators=[data_required()])
 	category = StringField('category', validators=[data_required()])
-	phone = TelField('phone number', validators=[data_required()])
+	phone = TelField('phone', validators=[data_required()])
 	email = EmailField('email')
 	website = URLField('website')
-	facebook = URLField('facebook page')
+	facebook = URLField('facebook')
 	address = StringField('address')
-	sentence = StringField('killer sentence about')
+	sentence = StringField('about')
 	submit = SubmitField('Create your business card now')
 
